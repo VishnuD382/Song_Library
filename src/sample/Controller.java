2dc27afab62009752a0356f7567173ab29d34c74
Vishnu <vishnudhanasekaran9@gmail.com>
2021-02-07 01:40:51 -0500

    Need to finish csv loading

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,54 @@
 package sample;
+import java.io.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Controller {
+    @FXML
+    ListView<String> listView;
+
+    private ObservableList<String> obsList;
+
+    String[] datatest = {"Chiefs", "Eagles" };
+
+
+
+
+    public void start(Stage mainStage){
+
+        int count = 0;
+        String file = "data.txt";
+        List<String[]> content = new ArrayList<>();
+        try(BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                System.out.println("printing");
+                content.add(line.split(","));
+            }
+        } catch (FileNotFoundException e) {
+            //Some error logging
+            System.out.println("File not found!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        obsList = FXCollections.observableArrayList(
+                "Giants",
+                "Patriots",
+                "49ers",
+                "Rams");
+
+        for (String s : datatest) {
+            obsList.add(s);
+        }
+
+        listView.setItems(obsList);
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,16 +4,31 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(
+                getClass().getResource("sample.fxml"));
+        AnchorPane root = (AnchorPane)loader.load();
+
+        Controller controller = loader.getController();
+        controller.start(primaryStage);
+
+
         primaryStage.setTitle("Hello World Testing");
-        primaryStage.setScene(new Scene(root, 300, 275));
+
+
+        Scene scene = new Scene(root, 300, 300);
+        primaryStage.setScene(scene);
         primaryStage.show();
+
     }
 
 
