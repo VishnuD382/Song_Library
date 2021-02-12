7ea4deafb00b4c0b519898122212557eb215f3ce
Vishnu <vishnudhanasekaran9@gmail.com>
2021-02-12 18:56:07 -0500

    working on add function

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,14 +1,22 @@
 package sample;
-import java.awt.*;
+
 import java.awt.event.ActionEvent;
 import java.io.*;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
+import javafx.scene.control.Dialog;
 import javafx.scene.control.TextField;
+
+import javafx.scene.layout.GridPane;
+import javafx.geometry.*;
 import javafx.stage.Stage;
+import javafx.util.Pair;
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -125,8 +133,57 @@ public class Controller {
     }
 
     public void insertSong(javafx.event.ActionEvent actionEvent) {
+        Dialog<Pair<String, String>> dialog = new Dialog<>();
+        dialog.setTitle("Add Song");
+        dialog.setHeaderText("Add a song to the List");
+
+        ButtonType addButtonType = new ButtonType("Add", ButtonBar.ButtonData.OK_DONE);
+        dialog.getDialogPane().getButtonTypes().addAll(addButtonType, ButtonType.CANCEL);
+
+        // Create the username and password labels and fields.
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(20, 150, 10, 10));
+
+        TextField name = new TextField();
+        name.setPromptText("Song Name");
+        TextField artist = new TextField();
+        artist.setPromptText("Song artist");
+        TextField album = new TextField();
+        album.setPromptText("Song album");
+        TextField year = new TextField();
+        year.setPromptText("Song year");
+
+
+        grid.add(new Label("Song Name:"), 0, 0);
+        grid.add(name, 1, 0);
+        grid.add(new Label("Song Artist:"), 0, 1);
+        grid.add(artist, 1, 1);
+        grid.add(new Label("Song album:"), 0, 2);
+        grid.add(album, 1, 2);
+        grid.add(new Label("Song Year:"), 0, 3);
+        grid.add(year, 1, 3);
+
+        // enabling and disabling add button
+        Node addButton = dialog.getDialogPane().lookupButton(addButtonType);
+        addButton.setDisable(true);
+
+        // Do some validation (using the Java 8 lambda syntax).
+        name.textProperty().addListener((observable, oldValue, newValue) -> {
+            artist.textProperty().addListener((observable1, oldValue1, newValue1) -> {
+                addButton.setDisable(newValue.trim().isEmpty());});
+        });
+
+
+        dialog.getDialogPane().setContent(grid);
+
+        Optional<Pair<String, String>> result = dialog.showAndWait();
+
+        result.ifPresent(usernamePassword -> {
+            System.out.println("Username=" + usernamePassword.getKey() + ", Password=" + usernamePassword.getValue());
+        });
 
         //obsList.add(txtAddItem.getText());
-        //System.out.println("Added: " + printtest);
     }
 }
